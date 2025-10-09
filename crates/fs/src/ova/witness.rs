bfdae19951211506a6c0b0a22672d5344c504656
winderica <winderica@gmail.com>
2025-10-10 04:39:50 +0800

    Refactor: start porting Ova

---
+++ crates/fs/src/ova/witness.rs
@@ -0,0 +1,26 @@
+use sonobe_primitives::{arithmetizations::ArithConfig, commitments::CommitmentDef, traits::Dummy};
+
+use crate::FoldingWitness;
+
+#[derive(Debug, PartialEq)]
+pub struct RunningWitness<CM: CommitmentDef> {
+    pub w: Vec<CM::Scalar>,
+    pub r: CM::Randomness,
+}
+
+impl<CM: CommitmentDef> FoldingWitness<CM> for RunningWitness<CM> {
+    const N_OPENINGS: usize = 1;
+
+    fn openings(&self) -> Vec<(&[CM::Scalar], &CM::Randomness)> {
+        vec![(&self.w, &self.r)]
+    }
+}
+
+impl<CM: CommitmentDef, Cfg: ArithConfig> Dummy<&Cfg> for RunningWitness<CM> {
+    fn dummy(cfg: &Cfg) -> Self {
+        Self {
+            w: vec![Default::default(); cfg.n_witnesses()],
+            r: Default::default(),
+        }
+    }
+}
