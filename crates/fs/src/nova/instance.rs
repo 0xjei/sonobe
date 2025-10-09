c64313b1f6a12a26421dac75a2d3fb59f234181e
winderica <winderica@gmail.com>
2025-10-10 04:39:50 +0800

    Refactor: start porting Nova

---
+++ crates/fs/src/nova/instance.rs
@@ -0,0 +1,89 @@
+use ark_ff::PrimeField;
+use sonobe_primitives::{
+    arithmetizations::ArithConfig, commitments::CommitmentDef, traits::Dummy,
+    transcripts::Absorbable,
+};
+
+use crate::FoldingInstance;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct RunningInstance<CM: CommitmentDef> {
+    pub cm_e: CM::Commitment,
+    pub u: CM::Scalar,
+    pub cm_w: CM::Commitment,
+    pub x: Vec<CM::Scalar>,
+}
+
+impl<CM: CommitmentDef> FoldingInstance<CM> for RunningInstance<CM> {
+    const N_COMMITMENTS: usize = 2;
+
+    fn commitments(&self) -> Vec<&CM::Commitment> {
+        vec![&self.cm_e, &self.cm_w]
+    }
+
+    fn public_inputs(&self) -> &[CM::Scalar] {
+        &self.x
+    }
+
+    fn public_inputs_mut(&mut self) -> &mut [CM::Scalar] {
+        &mut self.x
+    }
+}
+
+impl<CM: CommitmentDef, Cfg: ArithConfig> Dummy<&Cfg> for RunningInstance<CM> {
+    fn dummy(cfg: &Cfg) -> Self {
+        Self {
+            cm_e: Default::default(),
+            u: Default::default(),
+            cm_w: Default::default(),
+            x: vec![Default::default(); cfg.n_public_inputs()],
+        }
+    }
+}
+
+impl<CM: CommitmentDef> Absorbable for RunningInstance<CM> {
+    fn absorb_into<F: PrimeField>(&self, dest: &mut Vec<F>) {
+        self.u.absorb_into(dest);
+        self.x.absorb_into(dest);
+        self.cm_e.absorb_into(dest);
+        self.cm_w.absorb_into(dest);
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct IncomingInstance<CM: CommitmentDef> {
+    pub cm_w: CM::Commitment,
+    pub x: Vec<CM::Scalar>,
+}
+
+impl<CM: CommitmentDef> FoldingInstance<CM> for IncomingInstance<CM> {
+    const N_COMMITMENTS: usize = 1;
+
+    fn commitments(&self) -> Vec<&CM::Commitment> {
+        vec![&self.cm_w]
+    }
+
+    fn public_inputs(&self) -> &[CM::Scalar] {
+        &self.x
+    }
+
+    fn public_inputs_mut(&mut self) -> &mut [CM::Scalar] {
+        &mut self.x
+    }
+}
+
+impl<CM: CommitmentDef, Cfg: ArithConfig> Dummy<&Cfg> for IncomingInstance<CM> {
+    fn dummy(cfg: &Cfg) -> Self {
+        Self {
+            cm_w: Default::default(),
+            x: vec![Default::default(); cfg.n_public_inputs()],
+        }
+    }
+}
+
+impl<CM: CommitmentDef> Absorbable for IncomingInstance<CM> {
+    fn absorb_into<F: PrimeField>(&self, dest: &mut Vec<F>) {
+        self.x.absorb_into(dest);
+        self.cm_w.absorb_into(dest);
+    }
+}
---
+++ crates/fs/src/nova/witness.rs
@@ -0,0 +1,53 @@
+use sonobe_primitives::{arithmetizations::ArithConfig, commitments::CommitmentDef, traits::Dummy};
+
+use crate::FoldingWitness;
+
+#[derive(Debug, PartialEq)]
+pub struct RunningWitness<CM: CommitmentDef> {
+    pub e: Vec<CM::Scalar>,
+    pub r_e: CM::Randomness,
+    pub w: Vec<CM::Scalar>,
+    pub r_w: CM::Randomness,
+}
+
+impl<CM: CommitmentDef> FoldingWitness<CM> for RunningWitness<CM> {
+    const N_OPENINGS: usize = 2;
+
+    fn openings(&self) -> Vec<(&[CM::Scalar], &CM::Randomness)> {
+        vec![(&self.e, &self.r_e), (&self.w, &self.r_w)]
+    }
+}
+
+impl<CM: CommitmentDef, Cfg: ArithConfig> Dummy<&Cfg> for RunningWitness<CM> {
+    fn dummy(cfg: &Cfg) -> Self {
+        Self {
+            e: vec![Default::default(); cfg.n_constraints()],
+            r_e: Default::default(),
+            w: vec![Default::default(); cfg.n_witnesses()],
+            r_w: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct IncomingWitness<CM: CommitmentDef> {
+    pub w: Vec<CM::Scalar>,
+    pub r_w: CM::Randomness,
+}
+
+impl<CM: CommitmentDef> FoldingWitness<CM> for IncomingWitness<CM> {
+    const N_OPENINGS: usize = 1;
+
+    fn openings(&self) -> Vec<(&[CM::Scalar], &CM::Randomness)> {
+        vec![(&self.w, &self.r_w)]
+    }
+}
+
+impl<CM: CommitmentDef, Cfg: ArithConfig> Dummy<&Cfg> for IncomingWitness<CM> {
+    fn dummy(cfg: &Cfg) -> Self {
+        Self {
+            w: vec![Default::default(); cfg.n_witnesses()],
+            r_w: Default::default(),
+        }
+    }
+}
