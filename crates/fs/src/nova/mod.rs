086df2e05683fc57e2beef908c8891ec70c72a35
winderica <winderica@gmail.com>
2025-10-24 22:38:47 +0800

    Accumulation scheme compatible interface

--- crates/fs/src/nova/mod.rs
+++ crates/fs/src/nova/mod.rs
@@ -27,7 +27,8 @@ use witness::{IncomingWitness as IW, RunningWitness as RW};
 
 use crate::{
     DeciderKey, Error, FoldingSchemeDecider, FoldingSchemeDef, FoldingSchemeKeyGenerator,
-    FoldingSchemePreprocessor, FoldingSchemeProver, FoldingSchemeVerifier,
+    FoldingSchemePreprocessor, FoldingSchemeProver, FoldingSchemeVerifier, PlainInstance as PU,
+    PlainWitness as PW,
 };
 
 pub mod instance;
@@ -89,6 +90,19 @@ where
     }
 }
 
+impl<A, CM> Relation<PW<CM::Scalar>, PU<CM::Scalar>> for NovaKey<A, CM>
+where
+    A: ArithRelation<Vec<CM::Scalar>, Vec<CM::Scalar>>,
+    CM: CommitmentDef,
+{
+    type Error = Error;
+
+    fn check_relation(&self, w: &PW<CM::Scalar>, u: &PU<CM::Scalar>) -> Result<(), Self::Error> {
+        self.arith.check_relation(w, u)?;
+        Ok(())
+    }
+}
+
 impl<A, CM: CommitmentOps> WitnessInstanceSampler<IW<CM>, IU<CM>> for NovaKey<A, CM> {
     type Source = AssignmentsOwned<CM::Scalar>;
     type Error = Error;
@@ -97,6 +111,21 @@ impl<A, CM: CommitmentOps> WitnessInstanceSampler<IW<CM>, IU<CM>> for NovaKey<A,
         let (w, x) = (z.private, z.public);
         let (cm_w, r_w) = CM::commit(&self.ck, &w, rng)?;
         Ok((IW { w, r_w }, IU { cm_w, x }))
+    }
+}
+
+impl<A, CM: CommitmentDef> WitnessInstanceSampler<PW<CM::Scalar>, PU<CM::Scalar>>
+    for NovaKey<A, CM>
+{
+    type Source = AssignmentsOwned<CM::Scalar>;
+    type Error = Error;
+
+    fn sample(
+        &self,
+        z: Self::Source,
+        _rng: impl RngCore,
+    ) -> Result<(PW<CM::Scalar>, PU<CM::Scalar>), Error> {
+        Ok((z.private.into(), z.public.into()))
     }
 }
 
@@ -253,8 +282,8 @@ impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
     }
 }
 
-impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize> FoldingSchemeVerifier<1, 1>
-    for AbstractNova<CM, TF, CHALLENGE_BITS>
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
+    FoldingSchemeVerifier<1, 1> for AbstractNova<CM, TF, CHALLENGE_BITS>
 {
     fn verify(
         _vk: &(),
@@ -278,6 +307,170 @@ impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize> Fol
             u: U.u + rho,
             cm_w: U.cm_w + u.cm_w.mul(rho),
             x: cfg_iter!(U.x).zip(&u.x).map(|(a, b)| rho * b + a).collect(),
+        })
+    }
+}
+
+// used for the RO challenges.
+// From [Srinath Setty](https://microsoft.com/en-us/research/people/srinath/): In Nova, soundness
+// error ≤ 2/|S|, where S is the subset of the field F from which the challenges are drawn. In this
+// case, we keep the size of S close to 2^128.
+pub struct AbstractNova2<CM, TF, const CHALLENGE_BITS: usize = 128> {
+    _t: PhantomData<(CM, TF)>,
+}
+
+pub type Nova2<CM, const CHALLENGE_BITS: usize = 128> =
+    AbstractNova2<CM, <CM as CommitmentDef>::Scalar, CHALLENGE_BITS>;
+
+pub type CycleFoldNova2<CM, const CHALLENGE_BITS: usize = 128> =
+    AbstractNova2<CM, CF2<<CM as CommitmentDef>::Commitment>, CHALLENGE_BITS>;
+
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize> FoldingSchemeDef
+    for AbstractNova2<CM, TF, CHALLENGE_BITS>
+{
+    type CM = CM;
+    type RW = RW<CM>;
+    type RU = RU<CM>;
+    type IW = PW<CM::Scalar>;
+    type IU = PU<CM::Scalar>;
+
+    type TranscriptField = TF;
+    type Arith = R1CS<CM::Scalar>;
+
+    type Config = usize;
+    type PublicParam = CM::Key;
+    type DeciderKey = NovaKey<Self::Arith, CM>;
+    type Challenge = [bool; CHALLENGE_BITS];
+    type Proof<const M: usize, const N: usize> = (CM::Commitment, CM::Commitment);
+}
+
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
+    FoldingSchemePreprocessor for AbstractNova2<CM, TF, CHALLENGE_BITS>
+{
+    fn preprocess(ck_len: usize, mut rng: impl RngCore) -> Result<Self::PublicParam, Error> {
+        let ck = CM::generate_key(ck_len, &mut rng)?;
+        Ok(ck)
+    }
+}
+
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
+    FoldingSchemeKeyGenerator for AbstractNova2<CM, TF, CHALLENGE_BITS>
+{
+    fn generate_keys(ck: Self::PublicParam, r1cs: Self::Arith) -> Result<Self::DeciderKey, Error> {
+        let ck = Arc::new(ck);
+        let r1cs = Arc::new(r1cs);
+        let cfg = r1cs.config();
+        if ck.max_scalars_len() < cfg.n_constraints().max(cfg.n_witnesses()) {
+            return Err(Error::InvalidPublicParameters(
+                "The commitment key is too short for the R1CS instance".into(),
+            ));
+        }
+        Ok(Self::DeciderKey { arith: r1cs, ck })
+    }
+}
+
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
+    FoldingSchemeProver<1, 1> for AbstractNova2<CM, TF, CHALLENGE_BITS>
+{
+    fn prove(
+        pk: &NovaKey<Self::Arith, CM>,
+        transcript: &mut impl Transcript<TF>,
+        Ws: &[impl Borrow<Self::RW>; 1],
+        Us: &[impl Borrow<Self::RU>; 1],
+        ws: &[impl Borrow<Self::IW>; 1],
+        us: &[impl Borrow<Self::IU>; 1],
+        mut rng: impl RngCore,
+    ) -> Result<(Self::RW, Self::RU, Self::Proof<1, 1>, Self::Challenge), Error> {
+        let (W, U) = (Ws[0].borrow(), Us[0].borrow());
+        let (w, u) = (ws[0].borrow(), us[0].borrow());
+
+        // Compute the cross term `T` by following the optimized approach in
+        // [Mova](https://eprint.iacr.org/2024/1220.pdf)'s section 5.2.
+        let v = pk.arith.evaluate_at(AssignmentsOwned::from((
+            U.u + CM::Scalar::one(),
+            cfg_iter!(U.x).zip(&u[..]).map(|(a, b)| *a + b).collect(),
+            cfg_iter!(W.w).zip(&w[..]).map(|(a, b)| *a + b).collect(),
+        )))?;
+        let t = cfg_into_iter!(v)
+            .zip(&W.e)
+            .map(|(a, b)| a - b)
+            .collect::<Vec<_>>();
+
+        let (cm_w, r_w) = CM::commit(&pk.ck, w, rng)?;
+
+        // Use `StepRng::new(0, 0)`, which is a dummy RNG that always generates
+        // 0 for the randomness (i.e., `r_T = 0`), no matter whether `CM` itself
+        // is hiding or not.
+        //
+        // This is because in Nova, we don't need hiding property for commitment
+        // to `T`.
+        let (cm_t, r_t) = CM::commit(&pk.ck, &t, StepRng::new(0, 0))?;
+
+        let pi = (cm_w, cm_t);
+
+        let rho_bits = {
+            transcript.add(&U);
+            transcript.add(&u);
+            transcript.add(&pi);
+            transcript.challenge_bits(CHALLENGE_BITS)
+        };
+        let rho = CM::Scalar::from(<CM::Scalar as PrimeField>::BigInt::from_bits_le(&rho_bits));
+
+        Ok((
+            RW {
+                e: cfg_iter!(W.e).zip(&t).map(|(a, b)| rho * b + a).collect(),
+                r_e: W.r_e + r_t * rho,
+                w: cfg_iter!(W.w)
+                    .zip(&w[..])
+                    .map(|(a, b)| rho * b + a)
+                    .collect(),
+                r_w: W.r_w + r_w * rho,
+            },
+            RU {
+                cm_e: U.cm_e + cm_t.mul(rho),
+                u: U.u + rho,
+                cm_w: U.cm_w + cm_w.mul(rho),
+                x: cfg_iter!(U.x)
+                    .zip(&u[..])
+                    .map(|(a, b)| rho * b + a)
+                    .collect(),
+            },
+            pi,
+            rho_bits.try_into().unwrap(),
+        ))
+    }
+}
+
+impl<CM: GroupBasedCommitment, TF: SonobeField, const CHALLENGE_BITS: usize>
+    FoldingSchemeVerifier<1, 1> for AbstractNova2<CM, TF, CHALLENGE_BITS>
+{
+    fn verify(
+        _vk: &(),
+        transcript: &mut impl Transcript<TF>,
+        Us: &[impl Borrow<Self::RU>; 1],
+        us: &[impl Borrow<Self::IU>; 1],
+        pi: &Self::Proof<1, 1>,
+    ) -> Result<Self::RU, Error> {
+        let (U, u) = (Us[0].borrow(), us[0].borrow());
+
+        let rho_bits = {
+            transcript.add(&U);
+            transcript.add(&u);
+            transcript.add(pi);
+            transcript.challenge_bits(CHALLENGE_BITS)
+        };
+        let rho = CM::Scalar::from(<CM::Scalar as PrimeField>::BigInt::from_bits_le(&rho_bits));
+
+        let (cm_w, cm_t) = pi;
+
+        Ok(RU {
+            cm_e: U.cm_e + cm_t.mul(rho),
+            u: U.u + rho,
+            cm_w: U.cm_w + cm_w.mul(rho),
+            x: cfg_iter!(U.x)
+                .zip(&u[..])
+                .map(|(a, b)| rho * b + a)
+                .collect(),
         })
     }
 }
@@ -311,6 +504,28 @@ mod tests {
         )?;
 
         test_folding_scheme::<AbstractNova<Pedersen<G1Projective, false>, TF>, 1, 1>(
+            8,
+            CircuitForTest {
+                x: Fr::rand(&mut rng),
+            },
+            (0..rounds)
+                .map(|_| satisfying_assignments_for_test(Fr::rand(&mut rng)))
+                .collect(),
+            &mut rng,
+        )?;
+
+        test_folding_scheme::<AbstractNova2<Pedersen<G1Projective, true>, TF>, 1, 1>(
+            8,
+            CircuitForTest {
+                x: Fr::rand(&mut rng),
+            },
+            (0..rounds)
+                .map(|_| satisfying_assignments_for_test(Fr::rand(&mut rng)))
+                .collect(),
+            &mut rng,
+        )?;
+
+        test_folding_scheme::<AbstractNova2<Pedersen<G1Projective, false>, TF>, 1, 1>(
             8,
             CircuitForTest {
                 x: Fr::rand(&mut rng),
