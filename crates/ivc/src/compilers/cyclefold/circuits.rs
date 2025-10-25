70edc08bc8fe322871eb68e45c75ac401e472f07
winderica <winderica@gmail.com>
2025-10-25 23:27:13 +0800

    Initialize augmented circuit

---
+++ crates/ivc/src/compilers/cyclefold/circuits.rs
@@ -0,0 +1,66 @@
+use ark_crypto_primitives::sponge::poseidon::{PoseidonConfig, PoseidonSponge};
+use ark_ff::Zero;
+use ark_relations::gr1cs::{
+    ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, SynthesisError,
+};
+use ark_std::{marker::PhantomData, rand::RngCore};
+
+use sonobe_fs::{FoldingScheme, FoldingSchemeFullGadget, FoldingSchemePartialGadget};
+use sonobe_primitives::circuits::ConstraintSystemExt;
+use sonobe_primitives::relations::WitnessInstanceSampler;
+use sonobe_primitives::traits::SonobeField;
+use sonobe_primitives::transcripts::Transcript;
+use sonobe_primitives::{circuits::FCircuit, commitments::CommitmentDef};
+
+pub struct AugmentedCircuit<'a, FC: FCircuit, FS1, FS2> {
+    poseidon_config: PoseidonConfig<FC::Field>,
+    step_circuit: &'a FC,
+    _fs1: PhantomData<FS1>,
+    _fs2: PhantomData<FS2>,
+}
+
+impl<'a, FC: FCircuit, FS1, FS2> AugmentedCircuit<'a, FC, FS1, FS2>
+where
+    FS1: FoldingSchemePartialGadget<1, 1>,
+    FS2: FoldingSchemeFullGadget<1, 1>,
+{
+    fn compute_next_state(
+        &self,
+        cs: ConstraintSystemRef<FC::Field>,
+        pp_hash: FC::Field,
+        i: usize,
+        initial_state: &FC::State,
+        current_state: &FC::State,
+        external_inputs: FC::ExternalInputs,
+        U: FS1::RU,
+        u: FS1::IU,
+        hint: FS1::Hint,
+    ) -> Result<(FC::State, FC::ExternalOutputs), SynthesisError> {
+        todo!()
+    }
+}
+
+impl<'a, FC: FCircuit, FS1, FS2> ConstraintSynthesizer<FC::Field>
+    for AugmentedCircuit<'a, FC, FS1, FS2>
+where
+    FS1: FoldingSchemePartialGadget<1, 1>,
+    FS2: FoldingSchemeFullGadget<1, 1>,
+{
+    fn generate_constraints(
+        self,
+        cs: ConstraintSystemRef<FC::Field>,
+    ) -> Result<(), SynthesisError> {
+        self.compute_next_state(
+            cs,
+            Default::default(),
+            0,
+            &self.step_circuit.dummy_state(),
+            &self.step_circuit.dummy_state(),
+            todo!(),
+            todo!(),
+            todo!(),
+            todo!(),
+        )
+        .map(|_| ())
+    }
+}
