3f1ed979d8d11de982ec29384e2c3714d7b5135f
winderica <winderica@gmail.com>
2025-11-25 00:52:37 +0800

    Refactor code for in-circuit relation checks

--- crates/primitives/src/arithmetizations/r1cs/circuits.rs
+++ crates/primitives/src/arithmetizations/r1cs/circuits.rs
@@ -1,7 +1,7 @@
 use ark_ff::PrimeField;
 use ark_r1cs_std::alloc::{AllocVar, AllocationMode};
 use ark_relations::gr1cs::{Namespace, SynthesisError};
-use ark_std::{borrow::Borrow, marker::PhantomData, One};
+use ark_std::{borrow::Borrow, One};
 
 use super::R1CS;
 use crate::{
@@ -15,40 +15,37 @@ use crate::{
 };
 
 /// An in-circuit representation of the `R1CS` struct.
-///
-/// `M` is for the modulo operation involved in the satisfiability check when
-/// the underlying `FVar` is `NonNativeUintVar`.
 #[allow(non_snake_case)]
 #[derive(Debug, Clone)]
-pub struct R1CSMatricesVar<M, FVar> {
-    _m: PhantomData<M>,
+pub struct R1CSMatricesVar<FVar> {
     pub A: SparseMatrixVar<FVar>,
     pub B: SparseMatrixVar<FVar>,
     pub C: SparseMatrixVar<FVar>,
 }
 
 impl<F: PrimeField, ConstraintF: PrimeField, FVar: AllocVar<F, ConstraintF>>
-    AllocVar<R1CS<F>, ConstraintF> for R1CSMatricesVar<F, FVar>
+    AllocVar<R1CS<F>, ConstraintF> for R1CSMatricesVar<FVar>
 {
     fn new_variable<T: Borrow<R1CS<F>>>(
         cs: impl Into<Namespace<ConstraintF>>,
         f: impl FnOnce() -> Result<T, SynthesisError>,
-        _mode: AllocationMode,
+        mode: AllocationMode,
     ) -> Result<Self, SynthesisError> {
         f().and_then(|val| {
             let cs = cs.into();
 
+            let val = val.borrow();
+
             Ok(Self {
-                _m: PhantomData,
-                A: SparseMatrixVar::<FVar>::new_constant(cs.clone(), &val.borrow().A)?,
-                B: SparseMatrixVar::<FVar>::new_constant(cs.clone(), &val.borrow().B)?,
-                C: SparseMatrixVar::<FVar>::new_constant(cs.clone(), &val.borrow().C)?,
+                A: SparseMatrixVar::<FVar>::new_variable(cs.clone(), || Ok(&val.A), mode)?,
+                B: SparseMatrixVar::<FVar>::new_variable(cs.clone(), || Ok(&val.B), mode)?,
+                C: SparseMatrixVar::<FVar>::new_variable(cs.clone(), || Ok(&val.C), mode)?,
             })
         })
     }
 }
 
-impl<M, FVar> R1CSMatricesVar<M, FVar>
+impl<FVar> R1CSMatricesVar<FVar>
 where
     SparseMatrixVar<FVar>: MatrixGadget<FVar>,
     [FVar]: VectorGadget<FVar>,
@@ -69,11 +66,11 @@ where
     }
 }
 
-impl<M, FVar, WVar: AsRef<[FVar]>, UVar: AsRef<[FVar]>> ArithRelationGadget<WVar, UVar>
-    for R1CSMatricesVar<M, FVar>
+impl<FVar, WVar: AsRef<[FVar]>, UVar: AsRef<[FVar]>> ArithRelationGadget<WVar, UVar>
+    for R1CSMatricesVar<FVar>
 where
     SparseMatrixVar<FVar>: MatrixGadget<FVar>,
-    [FVar]: VectorGadget<FVar> + EquivalenceGadget<M>,
+    [FVar]: VectorGadget<FVar> + EquivalenceGadget<FVar>,
     FVar: Clone + One,
 {
     /// Evaluation is a tuple of two vectors (`AzBz` and `uCz`) instead of a
@@ -85,7 +82,7 @@ where
         self.eval_assignments((FVar::one(), u.as_ref(), w.as_ref()).into())
     }
 
-    fn enforce_evaluation(
+    fn check_evaluation(
         _w: &WVar,
         _u: &UVar,
         (lhs, rhs): Self::Evaluation,
