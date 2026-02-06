bb6f4fdc3a4f6f04efeb4a0bab20b2e670120aa6
winderica <winderica@gmail.com>
2026-02-06 18:35:53 +0800

    Actually test wasm targets

--- crates/fs/src/nova/mod.rs
+++ crates/fs/src/nova/mod.rs
@@ -258,11 +258,13 @@ impl<CM: GroupBasedCommitment, const CHALLENGE_BITS: usize> GroupBasedFoldingSch
 mod tests {
     use ark_bn254::{Fq, Fr, G1Projective};
     use ark_ff::UniformRand;
-    use ark_std::{error::Error, test_rng};
+    use ark_std::{error::Error, rand::thread_rng};
     use sonobe_primitives::{
         circuits::utils::{CircuitForTest, satisfying_assignments_for_test},
         commitments::pedersen::Pedersen,
     };
+    #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+    use wasm_bindgen_test::wasm_bindgen_test as test;
 
     use super::*;
     use crate::tests::test_folding_scheme;
@@ -341,7 +343,7 @@ mod tests {
 
     #[test]
     fn test_nova() -> Result<(), Box<dyn Error>> {
-        let mut rng = test_rng();
+        let mut rng = thread_rng();
 
         test_nova_opt::<Fr>(10, &mut rng)?;
         test_nova_opt::<Fq>(10, &mut rng)?;
