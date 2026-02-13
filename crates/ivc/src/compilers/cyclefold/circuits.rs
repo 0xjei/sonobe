0b6490079d24019b7d783082c1a4fef3ece65941
winderica <winderica@gmail.com>
2026-02-13 14:10:33 +0800

    Add FS & IVC docs

--- crates/ivc/src/compilers/cyclefold/circuits.rs
+++ crates/ivc/src/compilers/cyclefold/circuits.rs
@@ -1,3 +1,6 @@
+//! Augmented and CycleFold circuits for the CycleFold-based IVC compiler.
+
+use ark_ff::PrimeField;
 use ark_r1cs_std::{
     GR1CSVar,
     alloc::AllocVar,
@@ -6,22 +9,22 @@ use ark_r1cs_std::{
     fields::{FieldVar, fp::FpVar},
 };
 use ark_relations::gr1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
-use ark_std::marker::PhantomData;
 use sonobe_fs::{
     FoldingInstanceVar, FoldingSchemeFullVerifierGadget, FoldingSchemePartialVerifierGadget,
     GroupBasedFoldingSchemePrimary, GroupBasedFoldingSchemeSecondary,
 };
 use sonobe_primitives::{
-    algebra::Val,
     arithmetizations::Arith,
     circuits::FCircuit,
     commitments::CommitmentDef,
-    traits::{CF2, Dummy, SonobeCurve},
+    traits::{Dummy, SonobeCurve},
     transcripts::{Transcript, TranscriptGadget},
 };
 
 use crate::compilers::cyclefold::FoldingSchemeCycleFoldExt;
 
+/// [`AugmentedCircuit`] defines an augmented version of the user's step circuit
+/// which additionally verifies the folding proofs in-circuit.
 pub struct AugmentedCircuit<
     'a,
     FS1: GroupBasedFoldingSchemePrimary<1, 1>,
@@ -29,10 +32,10 @@ pub struct AugmentedCircuit<
     FC: FCircuit,
     T: Transcript<FC::Field>,
 > {
-    pub hash_config: T::Config,
-    pub arith1_config: &'a <FS1::Arith as Arith>::Config,
-    pub arith2_config: &'a <FS2::Arith as Arith>::Config,
-    pub step_circuit: &'a FC,
+    pub(super) hash_config: T::Config,
+    pub(super) arith1_config: &'a <FS1::Arith as Arith>::Config,
+    pub(super) arith2_config: &'a <FS2::Arith as Arith>::Config,
+    pub(super) step_circuit: &'a FC,
 }
 
 impl<'a, FS1, FS2, FC, T> AugmentedCircuit<'a, FS1, FS2, FC, T>
@@ -56,6 +59,9 @@ where
     FC: FCircuit<Field = <FS1::CM as CommitmentDef>::Scalar>,
     T: Transcript<FC::Field>,
 {
+    /// [`AugmentedCircuit::compute_next_state`] invokes the step circuit on the
+    /// current state and external inputs to compute the next state and external
+    /// outputs, and it additionally verifies the folding proofs in-circuit.
     #[allow(non_snake_case, clippy::too_many_arguments)]
     pub fn compute_next_state(
         &self,
@@ -95,6 +101,9 @@ where
         let cf_U = AllocVar::new_witness(cs.clone(), || Ok(cf_U))?;
         let cf_proofs = Vec::new_witness(cs.clone(), || Ok(cf_proofs))?;
 
+        // 1. Fold primary instances.
+        // 1.a. Derive the public input to the primary (augmented) circuit in
+        //      the `i-1`-th step, which is `u.x = H(i, z_0, z_i, U, cf_U)`.
         let u_x = sponge
             .clone()
             .add(&i)?
@@ -103,26 +112,47 @@ where
             .add(&U)?
             .add(&cf_U)?
             .get_field_element()?;
+        // 1.b. Construct the incoming instance `u` representing the `i-1`-th
+        //      execution of primary (augmented) circuit with the derived public
+        //      input.
         let u = FoldingInstanceVar::new_witness_with_public_inputs(cs.clone(), u, vec![u_x])?;
+        // 1.c. Fold the primary running instance `U` and incoming instance `u`
+        //      using the provided proof to obtain the next running instance
+        //      `UU`.
         let (UU, rho) = FS1::Gadget::verify_hinted(&(), &mut transcript, [&U], [&u], &proof)?;
+        // 1.d. If this is the base case (`i = 0`), then we should instead use
+        //      the dummy running instance as the next running instance.
         let actual_UU = is_basecase.select(&U_dummy, &UU)?;
 
+        // 2. Fold secondary instances.
         let mut cf_UU = cf_U;
         for ((cf_u, cf_u_x), cf_proof) in cf_us
             .iter()
+            // 2.a. Derive the public inputs to the secondary (CycleFold)
+            //      circuits in the `i`-th step, which are obtained by calling
+            //      the implementation of `FoldingSchemeCycleFoldExt`.
             .zip(FS1::to_cyclefold_inputs([U], [u], UU, proof, rho)?)
             .zip(&cf_proofs)
         {
+            // 2.b. Construct the incoming instance `cf_u` representing the
+            //      corresponding execution of secondary (CycleFold) circuit
+            //      with the derived public inputs.
             let cf_u =
                 FoldingInstanceVar::new_witness_with_public_inputs(cs.clone(), cf_u, cf_u_x)?;
+            // 2.c. Fold the secondary incoming instance `cf_u` into the running
+            //      instance `cf_UU` using the provided proof.
             cf_UU = FS2::Gadget::verify(&(), &mut transcript, [&cf_UU], [&cf_u], cf_proof)?;
         }
+        // 2.d. If this is the base case (`i = 0`), then we should instead use
+        //      the dummy running instance as the next running instance.
         let actual_cf_UU = is_basecase.select(&cf_U_dummy, &cf_UU)?;
 
+        // 3. Update state by invoking the step circuit.
         let (next_state, external_outputs) =
             self.step_circuit
                 .generate_step_constraints(i, current_state, external_inputs)?;
 
+        // 4. Compute public input `uu.x = H(i+1, z_0, z_{i+1}, UU, cf_UU)`.
         let uu_x = sponge
             .clone()
             .add(&ii)?
@@ -195,21 +225,18 @@ where
     }
 }
 
-/// [`CycleFoldConfig`] controls the behavior of [`CycleFoldCircuit`].
-///
-/// Looking ahead, the circuit computes the random linear combination of points,
-/// which is essentially done by iteratively computing `P = (P + p_i) * r_i`,
-/// where `P` is the folded point, `p_i` is the input point, and `r_i` is the
-/// randomness.
-pub trait CycleFoldConfig: Sized + Default {
-    type C: SonobeCurve;
-
-    /// `mark_point_as_public` marks a point as public.
+/// [`CycleFoldCircuit`] is the trait describing the deferred verification of
+/// the folding proofs which is now expressed as a circuit on the secondary
+/// curve.
+pub trait CycleFoldCircuit<F: PrimeField>: Sized + Default {
+    /// [`CycleFoldCircuit::mark_point_as_public`] marks a point as public.
     ///
     /// The final vector of public inputs is shorter than the result of calling
     /// [`AllocVar::new_input`], because we only need the x and y coordinates of
     /// the point, but the `infinity` flag is not necessary.
-    fn mark_point_as_public(point: &<Self::C as Val>::Var) -> Result<(), SynthesisError> {
+    fn mark_point_as_public<V: ToConstraintFieldGadget<F>>(
+        point: &V,
+    ) -> Result<(), SynthesisError> {
         for x in &point.to_constraint_field()?[..2] {
             // This line "converts" `x` from a witness to a public input.
             // Instead of directly modifying the constraint system, we explicitly
@@ -225,20 +252,9 @@ pub trait CycleFoldConfig: Sized + Default {
         Ok(())
     }
 
-    fn verify_point_rlc(&self, cs: ConstraintSystemRef<CF2<Self::C>>)
-    -> Result<(), SynthesisError>;
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct CycleFoldCircuit<Cfg> {
-    _cfg: PhantomData<Cfg>,
-}
-
-impl<Cfg: CycleFoldConfig> ConstraintSynthesizer<CF2<Cfg::C>> for CycleFoldCircuit<Cfg> {
-    fn generate_constraints(
-        self,
-        cs: ConstraintSystemRef<CF2<Cfg::C>>,
-    ) -> Result<(), SynthesisError> {
-        Cfg::default().verify_point_rlc(cs)
-    }
+    /// [`CycleFoldCircuit::verify_point_rlc`] verifies the deferred folding
+    /// proof in-circuit on the secondary curve, which is done by checking the
+    /// random linear combination of the commitments contained in the folding
+    /// instances.
+    fn verify_point_rlc(&self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError>;
 }
